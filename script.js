46c97470af402daf81eebcfac26c8183532d73bd
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-06-16 12:44:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-//your code here
-const bands = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'Aerosmith'];
-
-// Define the strip function to remove common words from band names
-function strip(word) {
-const regex = /^(a |the |an )/i;
-return word.replace(regex, '').trim();
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let obj={};
+function check(str){
+if(str[0]=='T' && str[1]=='h' && str[2]=='e' && str[3]==' '){
+var res=str.slice(4);
+obj[res]=str;
+return res;
 }
-
-// Sort the bands array using the strip function to ignore common words
-const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1);
-
-// Update the DOM with the sorted band names
-const bandList = document.getElementById('band');
-sortedBands.forEach(band => {
-const listItem = document.createElement('li');
-listItem.textContent = band;
-bandList.appendChild(listItem);
-});
+if(str[0]=='A'){
+if(str[1]==' '){
+var res=str.slice(2);
+obj[res]=str;
+return res;
+}
+if(str[1]=='n' && str[2]==' '){
+var res=str.slice(3);
+obj[res]=str;
+return res;
+}
+}
+obj[str]=str;
+return str;
+}
+for(let i=0;i<touristSpots.length;i++){
+touristSpots[i]=check(touristSpots[i]);
+// console.log(touristSpots[i]);
+}
+touristSpots.sort();
+for(let i=0;i<touristSpots.length;i++){
+touristSpots[i]=obj[touristSpots[i]];
+}
+console.log(touristSpots);
